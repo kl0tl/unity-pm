7209cb555c818909c9d47e47a58ccc9b6467f465
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2014-08-30 23:49:28 +0200

    style(home): indent

--- commands/home.js
+++ commands/home.js
@@ -5,8 +5,7 @@ var opn = require('opn');
 var nfcall = require('../lib/nfcall');
 var lookupCommand = require('./lookup');
 
-module.exports = function homeCommand(name) {
-  return lookupCommand(name).then(function (url) {
-    return nfcall(opn, url);
-  });
+module.exports = function homeCommand(name, options) {
+  return lookupCommand(name, options)
+    .then(function (url) { return nfcall(opn, url); });
 };
