a0c85eaf8c793aef8dd8b51161911b6f7eb641bb
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2014-08-30 23:54:16 +0200

    feat(version): add the `version` command

---
+++ commands/version.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var RcLoader = require('rcloader');
+
+var infoCommand = require('./info');
+var nfcall = require('../lib/nfcall');
+
+module.exports = function versionCommand(name, options) {
+  var g = options && options.global || false;
+  var method = g ? getGlobalPackageVersion : getInstalledPackageVersion;
+
+  return method(name);
+};
+
+function getGlobalPackageVersion(name) {
+  return infoCommand(name, { offline: true })
+    .then(function (res) { return res.version; });
+}
+
+function getInstalledPackageVersion(name) {
+  var loader = new RcLoader('.asset-store-packages');
+
+  return nfcall(loader.for, './')
+    .then(function (assetStorePackages) {
+      if (!(name in assetStorePackages.packages)) {
+        throw new Error();
+      }
+
+      return assetStorePackages.packages[name];
+    });
+}
