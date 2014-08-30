3e666dc49ca927c975ad6b187d47f9755954da38
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2014-08-30 23:51:28 +0200

    refactor(lookup): use `util.format()` instead of concatenation

--- commands/lookup.js
+++ commands/lookup.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var format = require('util').format;
+
 var infoCommand = require('./info');
 
-module.exports = function lookupCommand(name) {
-  return infoCommand(name).then(function (info) {
-    return info.url;
-  });
+module.exports = function lookupCommand(name, options) {
+  return infoCommand(name, options)
+    .then(function (info) {
+      return format('https://www.assetstore.unity3d.com/en/#!/content/%s', info.link.id);
+    });
 };
